bec411e40fbe95111a0267b454813aa69c68bec0
hartie95 <mail@hartie95.de>
2022-04-30 16:16:01 +0200

    updated for new sysdvr release and libtestla version

--- source/main.cpp
+++ source/main.cpp
@@ -3,7 +3,7 @@
 
 //This is a version for the SysDVR Config app protocol, it's not shown anywhere and not related to the major version
 #define SYSDVR_VERSION_MIN 5
-#define SYSDVR_VERSION_MAX 7
+#define SYSDVR_VERSION_MAX 8
 #define TYPE_MODE_USB 1
 #define TYPE_MODE_TCP 2
 #define TYPE_MODE_RTSP 4
